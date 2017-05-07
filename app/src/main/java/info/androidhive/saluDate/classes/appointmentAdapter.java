b2a57d7db65c6275403be56a96f5a3844f5d78f7
Luis Echenique <luisechenique114@gmail.com>
2017-05-07 02:13:30 -0500

    ListView+adapter datos fake
    cutre

---
+++ app/src/main/java/info/androidhive/saluDate/classes/appointmentAdapter.java
@@ -0,0 +1,42 @@
+package info.androidhive.saluDate.classes;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import info.androidhive.materialtabs.R;
+/**
+ * Created by Luis on 05/05/2017.
+ */
+
+public class appointmentAdapter extends ArrayAdapter<appointment> {
+    private int r;
+    private boolean phrases=false;
+    private int color;
+    public appointmentAdapter(Activity context, int resource){
+            super(context, resource);
+            r=resource;
+            }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+            View listItemView = convertView;
+            if (listItemView == null) {
+                listItemView = LayoutInflater.from(getContext()).inflate(
+                        r, parent, false);
+            }
+            appointment currentAppointment = getItem(position);
+            TextView specialty = (TextView) listItemView.findViewById(R.id.specialtyText);
+            TextView doctorN = (TextView) listItemView.findViewById(R.id.doctorText);
+            ImageView img = (ImageView) listItemView.findViewById(R.id.pimage);
+            specialty.setText(currentAppointment.getSpecialty());
+            doctorN.setText(currentAppointment.getDoctorName());
+            img.setImageResource(currentAppointment.getImage());
+
+            return listItemView;
+
+            }
+
+}
